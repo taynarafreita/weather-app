2257a74173b4339655a6d3df446faed0d274af49
taynara <taynara.freita98@gmail.com>
2023-10-17 07:19:37 -0300

    projeto finalizado

---
+++ src/app/modules/weather/page/weather-home/services/weather.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private apiKey = '559ba14ec5ed25c5d2ac64a49ea2235b'
+
+  constructor(private http: HttpClient) {}
+
+  getWeatherDatas(cityName: string): Observable<any> {
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`,
+    {}
+    );
+  }
+}
